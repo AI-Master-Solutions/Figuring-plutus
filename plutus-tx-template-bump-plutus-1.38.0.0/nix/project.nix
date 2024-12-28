1ab1dfa40ac7e5fdfb02afd9763615e10353cb9a
AI-Master-Solutions <aimastersolutionscolombia@gmail.com>
2024-12-28 17:15:42 -0500

    .

--- plutus-tx-template-bump-plutus-1.38.0.0/nix/project.nix
+++ plutus-tx-template-bump-plutus-1.38.0.0/nix/project.nix
@@ -7,38 +7,60 @@ let
       # When `isCross` is `true`, it means that we are cross-compiling the project.
       # WARNING You must use the `pkgs` coming from cabalProject' for `isCross` to work.
       isCross = pkgs.stdenv.hostPlatform != pkgs.stdenv.buildPlatform;
-    in
-    {
+    in {
       src = ../.;
 
       shell.withHoogle = false;
 
       inputMap = {
-        "https://chap.intersectmbo.org/" = inputs.CHaP;
+        "https://chap.intersectmbo.org/" = inputs.iogx.inputs.CHaP;
       };
 
-      name = "plutus-tx-template";
+      name = "my-project";
 
       compiler-nix-name = lib.mkDefault "ghc96";
 
-      modules =
-        [
-          {
-            packages = { };
-          }
-        ];
+      # flake.variants.profiled = {
+      #   modules = [{
+      #     enableProfiling = true;
+      #     enableLibraryProfiling = true;
+      #   }];
+      # };
+
+      # flake.variants.ghc928 = {
+      #   compiler-nix-name = "ghc928";
+      # };
+
+      # flake.variants.ghc8107 = {
+      #   compiler-nix-name = "ghc8107";
+      # };
+
+      modules = [ { packages = { }; } { packages = { }; } ];
     });
 
-
   cabalProject = cabalProject'.appendOverlays [ ];
-
 
   # Docs for mkHaskellProject: https://github.com/input-output-hk/iogx/blob/main/doc/api.md#mkhaskellproject
   project = lib.iogx.mkHaskellProject {
     inherit cabalProject;
+
     shellArgs = repoRoot.nix.shell;
+
+    # includeMingwW64HydraJobs = false;
+
+    # includeProfiledHydraJobs = false;
+
+    # readTheDocs = {
+    #   enable = false;
+    #   siteFolder = "doc/read-the-docs-site";
+    #   sphinxToolchain = null;
+    # };
+
+    # combinedHaddock = {
+    #   enable = false;
+    #   prologue = "";
+    #   packages = [];
+    # };
   };
 
-in
-
-project
+in project
