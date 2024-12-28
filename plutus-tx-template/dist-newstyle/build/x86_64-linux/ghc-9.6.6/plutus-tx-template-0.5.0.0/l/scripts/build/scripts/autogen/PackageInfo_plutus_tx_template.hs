3bf17964d670b6aba80a38bb7186d2e44c6f9cc7
AI-Master-Solutions <aimastersolutionscolombia@gmail.com>
2024-12-28 17:38:37 -0500

    .

---
+++ plutus-tx-template/dist-newstyle/build/x86_64-linux/ghc-9.6.6/plutus-tx-template-0.5.0.0/l/scripts/build/scripts/autogen/PackageInfo_plutus_tx_template.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -w #-}
+module PackageInfo_plutus_tx_template (
+    name,
+    version,
+    synopsis,
+    copyright,
+    homepage,
+  ) where
+
+import Data.Version (Version(..))
+import Prelude
+
+name :: String
+name = "plutus_tx_template"
+version :: Version
+version = Version [0,5,0,0] []
+
+synopsis :: String
+synopsis = ""
+copyright :: String
+copyright = ""
+homepage :: String
+homepage = ""
